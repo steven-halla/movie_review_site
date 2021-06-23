bff6321017f87c5b27db9fd5cf74de9b622f5fa5
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-06-23 12:10:54 -0700

    add google analytics to site

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
+import GA4React from "ga-4-react";
 import reportWebVitals from './reportWebVitals';
+const ga4react = new GA4React('G-6Z1FJE4EX5');
 
+(async () => {
+  await ga4react.initialize();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-;
+  ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
