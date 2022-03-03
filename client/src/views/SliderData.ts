e887005ba8fedfcd00771d976d09bd5e1fe6f022
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-02 17:25:24 -0800

    "improving header and home page"

---
+++ client/src/views/SliderData.ts
@@ -0,0 +1,15 @@
+
+
+import {SliderImage} from "./ImageSlider";
+
+export const SliderData: SliderImage[] = [
+  {
+    image: 'https://cdn.mos.cms.futurecdn.net/isNjbTGFgJPqhXEa6RxLcZ.jpg'
+  },
+
+  {
+    image: 'https://www.avpcentral.com/images/colonial-marine-teams/uss-sulaco-colonial-marines.jpg'
+  }
+];
+
+
