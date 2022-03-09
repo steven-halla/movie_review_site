72d95a1349b5e3e4404208d6aef05e1e8aacf301
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-09 00:20:14 -0800

    "working on back end for file uploader"

--- server/routes/fileuploader.js
+++ server/routes/fileuploader.js
@@ -6,13 +6,15 @@ const app = express();
 app.use(fileUpload());
 
 // Upload Endpoint
-app.post('/avatarupload', (req, res) => {
+//change this to reflect need for a user id
+app.post('/users/id/profile/avatar', (req, res) => {
   if (req.files === null) {
     return res.status(400).json({ msg: 'No file uploaded' });
   }
 
   const file = req.files.file;
   console.log(`${__dirname}/client/public/uploads/${file.name}`);
+  //change the function below to write to an S3 bucket
   file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
     if (err) {
       console.error(err);
