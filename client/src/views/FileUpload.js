967ab75c3a17c05a9933673e6758b9c0050d2f58
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-08 18:01:28 -0800

    "file upload component added"

---
+++ client/src/views/FileUpload.js
@@ -0,0 +1,73 @@
+import React , { useState } from 'react';
+import axios from 'axios';
+import { Message } from "./Message";
+import { Progress } from "./Progress";
+
+export const FileUpload = () => {
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+
+  const onChange = e => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'content-Type': 'multipart/form-data'
+        },
+        onUploadProgress: progressEvent => {
+          setUploadPercentage(
+            parseInt(
+              Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            )
+          );
+          setTimeout(() => setUploadPercentage(0), 10000);
+        }
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({fileName, filePath});
+      // setUploadedFile({fileName: fileName, filePath: filePath});
+      setMessage('File uploaded')
+    }catch(err) {
+      if(err.response.status === 500) {
+        setMessage("there was a problem with the server");
+      } else {
+        setMessage(err.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <div >
+      {message ? <Message msg={message} /> : null }
+      <form onSubmit={onSubmit} action="">
+        <div className="custom-file mb-4">
+          <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
+          <label className="custom-file-label" htmlFor="customFile">
+            {filename}
+          </label>
+        </div>
+        <Progress percentage={uploadPercentage}/>
+        <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
+      </form>
+      { uploadedFile ? <div className="row mt-5 " >
+        <div className="col-md-6 m-auto">
+          <h3 className="text-center">{ uploadedFile.fileName } </h3>
+          <img style={{ width: '100%' }} src={uploadedFile.filePath} alt=""/>
+        </div>
+      </div> : null }
+    </div>
+  );
+};
