d3c7d5ba568e0d08dbdcb0a0e345169894838860
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-10 18:58:38 -0800

    "refactor"

--- server/routes/fileuploader.js
+++ server/routes/fileuploader.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+const uploadFile = (fileName) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: avatar-image-uploader,
+    Key: '', // File name you want to save as in S3
+    Body: fileContent
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
 
 const app = express();
 
